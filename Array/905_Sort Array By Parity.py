27b85ac08667f0cf3893453f73c328f80da8fdd2
Heyingming <944426045@qq.com>
2018-11-19 19:50:10 +0800

    学到了新的方法~

---
+++ Array/905_Sort Array By Parity.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        return sorted(A, key = lambda x : x % 2)
+
+solution = Solution()
+print solution.sortArrayByParity([3,1,2,4])
