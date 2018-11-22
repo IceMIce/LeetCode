38b90bdaf3078cd0cbc9fd6d9d537fa230939ee7
Heyingming <944426045@qq.com>
2018-11-22 22:49:44 +0800

    加班~难受

---
+++ Array/724_Find Pivot Index.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ret = sum(nums)
+        left = 0
+        for k, v in enumerate(nums):
+            if left * 2 + v == ret:
+                return k
+            left += v
+        return -1
+
+solution = Solution()
+print solution.pivotIndex([1, 1])
